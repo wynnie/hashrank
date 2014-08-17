64d3a88af3cc495252ec5eaf964f1630db088f4d
Vinny <wynnie@users.noreply.github.com>
2014-08-17 13:54:26 -0700

    Update hashRank.h

--- hashRank.h
+++ hashRank.h
@@ -1,5 +1,5 @@
 /*
-hashRank.h : By n3m3sys, Aug-2008
+hashRank.h :  wynnie, Aug-2008
 
 hashRank is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
